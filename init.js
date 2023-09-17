8049db806e33ccde30ce77cb33eff425acaf4c74
Ryan Jacundina <120759214+RyanJacundina@users.noreply.github.com>
2023-09-17 16:56:30 -0300

    Update init.js

--- init.js
+++ init.js
@@ -1,6 +1,21 @@
+document.addEventListener("click", ()=>{
+var elemento = document.documentElement; // Isso geralmente representa a raiz do documento, ou seja, a página inteira
+
+// Solicita o modo de tela cheia
+if (elemento.requestFullscreen) {
+  elemento.requestFullscreen();
+} else if (elemento.mozRequestFullScreen) { // Para navegadores Firefox
+  elemento.mozRequestFullScreen();
+} else if (elemento.webkitRequestFullscreen) { // Para navegadores WebKit (Chrome, Safari)
+  elemento.webkitRequestFullscreen();
+} else if (elemento.msRequestFullscreen) { // Para navegadores Internet Explorer
+  elemento.msRequestFullscreen();
+}
+});
+
 (function() {
 
-  document.body.style.zoom = "50%";
+  document.body.style.zoom = "100%";
 
   const overworld = new Overworld({
     element: document.querySelector('body'),
